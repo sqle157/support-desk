689fd48b107e8dfa0f833e25d4edef50e3f5b15a
Sang Le <57263682+sqle157@users.noreply.github.com>
2022-04-18 21:17:42 -0600

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,43 @@
-console.log('Server...');
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// allow send raw json
+app.use(express.json());
+// allow send urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({ message: 'Welcome to the Support Desk API' });
+	});
+}
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { getNotes, createNote } from '../features/notes/noteSlice';
+import { toast } from 'react-toastify';
+import Modal from 'react-modal';
+import { FaPlus } from 'react-icons/fa';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import NoteItem from '../components/NoteItem';
+
+const customStyles = {
+	content: {
+		width: '600px',
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+		position: 'relative',
+	},
+};
+
+Modal.setAppElement('#root');
+
+function Ticket() {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [noteText, setNoteText] = useState('');
+	const { ticket, isLoading, isError, message } = useSelector((state) => state.ticket);
+	const { notes, isLoading: notesIsLoading } = useSelector((state) => state.notes);
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const { ticketId } = useParams();
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+
+		dispatch(getTicket(ticketId));
+		dispatch(getNotes(ticketId));
+	}, [dispatch, isError, message, ticketId]);
+
+	// Closed Ticket
+	const onTicketClosed = () => {
+		dispatch(closeTicket(ticketId));
+		toast.success('Ticket Closed');
+		navigate('/tickets');
+	};
+
+	// Create note submit
+	const onNoteSubmit = (e) => {
+		e.preventDefault();
+		dispatch(createNote({ noteText, ticketId }));
+		closeModal();
+	};
+
+	// Open/Modal
+	const openModal = () => setModalIsOpen(true);
+	const closeModal = () => setModalIsOpen(false);
+
+	if (isLoading || notesIsLoading) {
+		return <Spinner />;
+	}
+
+	if (isError) {
+		return <h3>Something went wrong...</h3>;
+	}
+
+	return (
+		<div className='ticket-page'>
+			<header className='ticket-header'>
+				<BackButton url='/tickets' />
+				<h2>
+					Ticket ID: {ticket._id}
+					<span className={`status status-${ticket.status}`}>{ticket.status}</span>
+				</h2>
+				<h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
+				<h3>Product: {ticket.product}</h3>
+				<hr />
+				<div className='ticket-desc'>
+					<h3>Description of Issue</h3>
+					<p>{ticket.description}</p>
+				</div>
+				<h2>Notes</h2>
+			</header>
+
+			{ticket.status !== 'closed' && (
+				<button className='btn' onClick={openModal}>
+					<FaPlus /> Add Note
+				</button>
+			)}
+
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel='Add Note'>
+				<h2>Add Note</h2>
+				<button className='btn-close' onClick={closeModal}>
+					X
+				</button>
+				<form onSubmit={onNoteSubmit}>
+					<div className='form-group'>
+						<textarea
+							name='noteText'
+							id='noteText'
+							className='form-control'
+							placeholder='Note Text'
+							value={noteText}
+							onChange={(e) => setNoteText(e.target.value)}></textarea>
+					</div>
+					<div className='form-group'>
+						<button type='submit' className='btn'>
+							Submit
+						</button>
+					</div>
+				</form>
+			</Modal>
+
+			{notes.map((note) => (
+				<NoteItem key={note._id} note={note} />
+			))}
+
+			{ticket.status !== 'closed' && (
+				<button onClick={onTicketClosed} className='btn btn-block btn-danger'>
+					Close Ticket
+				</button>
+			)}
+		</div>
+	);
+}
+export default Ticket;
